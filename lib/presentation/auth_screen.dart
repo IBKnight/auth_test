c4457841e9dd0e6b9e49d38f1517a1a6fb63974d
IBKnight <youtube.integral@mail.ru>
2024-03-03 23:24:31 +0300

    added validation

--- lib/presentation/auth_screen.dart
+++ lib/presentation/auth_screen.dart
@@ -10,39 +10,70 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   final TextEditingController _loginController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+     resizeToAvoidBottomInset: true,
       body: Center(
         child: SizedBox(
-          width: MediaQuery.of(context).size.width * .5,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Auth',
-                style: TextStyle(fontSize: 30),
-              ),
-              const SizedBox(height: 32),
-              TextFormField(
-                controller: _loginController,
-                decoration: const InputDecoration(
-                  labelText: 'Login',
-                  border: OutlineInputBorder(),
+          width: MediaQuery.of(context).size.width * .9,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Auth',
+                  style: TextStyle(fontSize: 30),
                 ),
-              ),
-              const SizedBox(height: 32),
-              TextFormField(
-                controller: _passwordController,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
+                const SizedBox(height: 32),
+                TextFormField(
+                  controller: _loginController,
+                  decoration: const InputDecoration(
+                    labelText: 'Login',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (String? value) {
+                    value = value ?? '';
+                    if (value.length < 7) {
+                      return 'Username must be at least 7 characters long';
+                    }
+
+                    final regex = RegExp(r'^[a-zA-Z0-9]+$');
+
+                    if (!regex.hasMatch(value)) {
+                      return 'Username must only contain alphanumeric characters';
+                    }
+
+                    return null;
+                  },
                 ),
-              ),
-              const SizedBox(height: 32),
-              ElevatedButton(onPressed: () {}, child: const Text('Log in'))
-            ],
+                const SizedBox(height: 32),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (String? value) {
+                    value = value ?? '';
+                    if (value.length < 7) {
+                      return 'Password must be at least 7 characters long';
+                    }
+
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 32),
+                ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {}
+                    },
+                    child: const Text('Log in'))
+              ],
+            ),
           ),
         ),
       ),
